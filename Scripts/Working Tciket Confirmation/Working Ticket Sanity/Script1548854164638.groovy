6ffaeff18dbfb25c0192b84186c0294a20564073
Lineagent <leon.b@y-it.co.il>
2019-01-31 11:34:05 +0200

    kiosk managment almost finish

--- Scripts/Working Tciket Confirmation/Working Ticket Sanity/Script1548854164638.groovy
+++ Scripts/Working Tciket Confirmation/Working Ticket Sanity/Script1548854164638.groovy
@@ -12,8 +12,7 @@ import com.kms.katalon.core.testobject.TestObject as TestObject
 import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import internal.GlobalVariable as GlobalVariable
-
-import com.kms.katalon.core.testobject.ConditionType
+import com.kms.katalon.core.testobject.ConditionType as ConditionType
 
 CustomKeywords.'login.c_login.logIn'(site, userName, userPass)
 
@@ -79,22 +78,106 @@ WebUI.waitForElementPresent(findTestObject('Driver Ticket Confirmation/specificD
 
 WebUI.verifyElementPresent(findTestObject('Driver Ticket Confirmation/specificDateTable'), 5)
 
+int i = 0
+
+int j = 0
+
+int ij = 0
+
 int validCount = 0
 
+int validCountMax = 3
+
+TestObject driversTableRow = new TestObject('Empty')
+TestObject datesTableRow = new TestObject('Empty')
+TestObject specificDateTableRow = new TestObject('Empty')
+
+while (validCount < validCountMax && i < 20) {
+    j = 0
+
+    driversTableRow = new TestObject('driversTableRow' + i)
+
+    driversTableRow.addProperty('xpath', ConditionType.EQUALS, f_driver(i))
+
+    WebUI.delay(1)
+
+    WebUI.scrollToElement(driversTableRow, 1)
+
+    WebUI.click(driversTableRow)
+	
+	while (validCount < validCountMax && j < 20) {
+		
+		ij = 0
+		
+		datesTableRow = new TestObject('datesTableRow' + j)
+		
+		datesTableRow.addProperty('xpath', ConditionType.EQUALS, f_dates(j))
+		
+		WebUI.delay(1)
+		
+		try {
+			
+			WebUI.scrollToElement(datesTableRow, 1)
+			
+			WebUI.click(datesTableRow)
+			
+			while (validCount < validCountMax && ij < 3) {
+				
+				specificDateTableRow = new TestObject('specificDateRow' + ij)
+				
+				specificDateTableRow.addProperty('xpath', ConditionType.EQUALS, f_specidicDate(ij))
+				
+				try {
+					
+					boolean elemPresent = WebUI.waitForElementVisible(specificDateTableRow, 1)
+					
+					if (elemPresent) {
+						
+						WebUI.verifyElementVisibleInViewport(specificDateTableRow, 1)
+						
+					} else {
+					
+						break
+						
+					}
+					
+					ij++
+					
+					if (ij == 3) validCount++
+					
+				} catch (e) {
+				
+					break
+				}
+			}
+			
+		} catch (e) {
+			
+			break
+			
+		} finally {
+		
+			j++
+		}
+	}
+
+    i++
+
+}
+
 String f_driver(int rowNum) {
-	 return ".//*[@id='managementKioskview']//div[2]//div[1]//div[2]//div[1]//div[2]//div[1]//table//tr[" + (rowNum + 1) + "]"
-}
-
-int i = 0
-while (validCount < 3) {
-	TestObject driversTableRow = new TestObject("driversTableRow" + (i))
-	driversTableRow.addProperty("xpath", ConditionType.EQUALS, f_driver(i))
-	WebUI.delay(1)
-	WebUI.click(driversTableRow)
-	i++
-	validCount++
+    return ".//*[@id='managementKioskview']//div[2]//div[1]//div[2]//div[1]//div[2]//div[1]//table//tr[" + (rowNum + 
+    1) + "]"
 }
 
 
+String f_dates(int rowNum) {
+	return ".//*[@id='managementKioskview']//div[2]//div[1]//div[4]//div[1]//div[2]//table//tr[" + (rowNum +
+	1) + "]"
+}
 
+String f_specidicDate(int rowNum) {
+	return ".//*[@id='managementKioskview']/div[1]/div[2]/div[2]/div[1]/div[3]/div[1]/div[2]//table//tr[" + (rowNum +
+	1) + "]"
+}
 
